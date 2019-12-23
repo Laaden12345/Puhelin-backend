7e9665e526c2e815240bd6f241acd5a7929279f6
Lassi <lassi.knuuttila@taitounited.fi>
2019-12-23 23:58:49 +0200

    Tehty 3.8

---
+++ index.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :body',
+  ),
+);
+
+let persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4,
+  },
+];
+
+app.get('/info', (request, response) => {
+  const contents = `<div>Phonebook has info for ${persons.length} contacts</div>`;
+  const date = '</div>' + new Date().toString() + '</div>';
+  response.write(contents);
+  response.write(date);
+  response.end();
+});
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing',
+    });
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing',
+    });
+  }
+  if (persons.find(person => person.name == body.name)) {
+    return response.status(400).json({
+      error: 'name already exists',
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 10000),
+  };
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const person = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
