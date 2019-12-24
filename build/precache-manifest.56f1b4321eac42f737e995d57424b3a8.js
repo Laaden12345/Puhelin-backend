1ff4efefd46bf3b9c976ef8eb0ffeffaef571722
Lassi <lassi.knuuttila@taitounited.fi>
2019-12-24 13:54:17 +0200

    uibuild

---
+++ build/precache-manifest.56f1b4321eac42f737e995d57424b3a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91847f5b0babd92b056aece4fa7fe279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2bb9e5d139bc564ea4b",
+    "url": "/static/css/main.736e81bf.chunk.css"
+  },
+  {
+    "revision": "66386f571d4f0e7a511b",
+    "url": "/static/js/2.47b15b73.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.47b15b73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2bb9e5d139bc564ea4b",
+    "url": "/static/js/main.6f493e8d.chunk.js"
+  },
+  {
+    "revision": "57d8af95778078760a0e",
+    "url": "/static/js/runtime-main.283795fd.js"
+  }
+]);
