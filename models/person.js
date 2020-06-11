4300f044e1fe0c9cd1ed54ecc404c2aa2a93cebb
Lassi <lassi.knuuttila@taitounited.fi>
2020-06-12 01:24:24 +0300

    3.22 linttaus tehty

--- models/person.js
+++ models/person.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+	.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() =>{
+		console.log('connected to MongoDB')
+	})
+	.catch((error) =>{
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: 3,
-  },
-  number: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
-});
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+		minlength: 3,
+	},
+	number: {
+		type: String,
+		required: true,
+		minlength: 8,
+	},
+})
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+	transform: (document, returnedObject) =>{
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	},
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
